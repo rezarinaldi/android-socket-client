ae4ab28d983aba16f5e1c7b6ba62928764a73cd3
rezarinaldi <rez4rinaldi@gmail.com>
2020-11-28 17:06:12 +0700

    update backend, Alhamdulillah done

--- app/src/main/java/com/rezarinaldi/socketclientandroid/MainActivity.java
+++ app/src/main/java/com/rezarinaldi/socketclientandroid/MainActivity.java
@@ -2,13 +2,126 @@ package com.rezarinaldi.socketclientandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
 
+@SuppressLint("SetTextI18n")
 public class MainActivity extends AppCompatActivity {
+    Thread Thread1 = null;
+    EditText edtIP, edtPort, edtMessage;
+    TextView tvMessage;
+    Button btnSend, btnConnect;
+    String SERVER_IP;
+    int SERVER_PORT;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        edtIP = findViewById(R.id.edtIPClient);
+        edtPort = findViewById(R.id.edtPortClient);
+        edtMessage = findViewById(R.id.edtPesan);
+        tvMessage = findViewById(R.id.tvPesan);
+        btnSend = findViewById(R.id.btnKirim);
+        btnConnect = findViewById(R.id.btnKonek);
+
+        btnConnect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tvMessage.setText("");
+                SERVER_IP = edtIP.getText().toString().trim();
+                SERVER_PORT = Integer.parseInt(edtPort.getText().toString().trim());
+                Thread1 = new Thread(new Thread1());
+                Thread1.start();
+            }
+        });
+
+        btnSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String message = edtMessage.getText().toString().trim();
+                if (!message.isEmpty()) {
+                    new Thread(new Thread3(message)).start();
+                }
+            }
+        });
+    }
+
+    private PrintWriter output;
+    private BufferedReader input;
+    class Thread1 implements Runnable {
+        @Override
+        public void run() {
+            Socket socket;
+            try {
+                socket = new Socket(SERVER_IP, SERVER_PORT);
+                output = new PrintWriter(socket.getOutputStream());
+                input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        tvMessage.setText("Connected\n");
+                    }
+                });
+                new Thread(new Thread2()).start();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    class Thread2 implements Runnable {
+        @Override
+        public void run() {
+            while (true) {
+                try {
+                    final String message = input.readLine();
+                    if (message != null) {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                tvMessage.append("Server: " + message + "\n");
+                            }
+                        });
+                    } else {
+                        Thread1 = new Thread(new Thread1());
+                        Thread1.start();
+                        return;
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    class Thread3 implements Runnable {
+        private String message;
+        Thread3(String message) {
+            this.message = message;
+        }
+
+        @Override
+        public void run() {
+            output.write(message);
+            output.flush();
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    tvMessage.append("Client: " + message + "\n");
+                    edtMessage.setText("");
+                }
+            });
+        }
     }
 }
